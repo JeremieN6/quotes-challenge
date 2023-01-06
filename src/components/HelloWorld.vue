d1f1cbdad1bd1b6b971e74eacde0035215c0a657
JeremieN6 <ngoyi.jeremie@gmail.com>
2023-01-06 19:57:22 +0100

    Add style + api

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,186 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <!-- <div class="hello">
+    <div v-for="item in quotes" v-bind:key="item._id" >
+      <p style="color: blue;">{{item.content}}</p>
+      <span style="color: red;">{{ item.author }}</span>
+      </div>
+  </div> -->
+          <div class="myheader">
+            <div class="random" v-on:click="newQuote">
+                {{refresh_quote}}
+                <i class="fa fa-refresh" aria-hidden="true"></i>
+            </div>
+        </div>
+        <div class="container_page" v-for="item in quotes" v-bind:key="item._id" >                
+            <div class="author" id="containerAuthor" v-show="false">
+                {{item.author}}
+            </div>
+            <div class="container_quotes">
+                <div class="quote">
+                    "{{item.content}}"
+                </div>
+                <div class="container_author_genre" id="container_author_genre" v-on:click="test">
+                    <div class="author_genre" v-for="item in quotes" v-bind:key="item.tags">
+                        <div class="theAuthor">{{item.author}}</div>
+                        <div class="genre">{{item.tags}}</div>
+                    </div>
+                    <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+                </div>
+            </div>    
+        </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data(){
+    return{
+      quotes: null,
+      refresh_quote:"random",
+    }
+  },
+    methods:{
+      test: function(){
+          var container_author_genre = document.getElementById("container_author_genre");
+          var containerAuthor = document.getElementById("containerAuthor");
+
+          container_author_genre.style.visibility = "none";
+          containerAuthor.style.display = "flex";
+      },
+      newQuote: function(){
+        window.location.reload();
+      }
+  },
+  mounted(){
+    axios
+    .get("https://api.quotable.io/random")
+    .then((reponse) => {
+      this.quotes = reponse;
+      console.log(this.quotes);
+    })
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+@import url('https://fonts.cdnfonts.com/css/raleway-5');
+@import url('https://fonts.cdnfonts.com/css/montserrat');
+
+:root{
+    /*COLOR*/
+    --main-color: #333333;
+    --second-color: #828282;
+    --white-color: #FFFFFF;
+    --third-color: #F7DF94;
+    /*FONTS*/
+    --main-font: 'Raleway';
+    --footer-font: 'Montserrat';
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.myheader{
+    display: flex;
+    justify-content: flex-end;
+    margin: 1em 1em 0 0;
+    position: absolute;
+    top: 0;
+    right: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.container_page{
+    position: absolute;
+    top: 30%;
 }
-a {
-  color: #42b983;
+.author{
+    display: none;
+    justify-content: flex-start;
+    align-items: flex-start;
+    margin: 0em 2em 2em;
+    padding-left: 1.5em;
+    font-family: var(--main-font);
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 42px;
+    color: var(--main-color);
+}
+.random{
+    font-family: var(--main-font);
+    font-style: normal;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 120%;
+    color: var(--main-color);
+    cursor: pointer;
+}
+.fa-refresh{
+    color: var(--main-color);
+    transform: rotate(90deg);
+}
+.container_quotes{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin: 0 0 5em 0;
+    font-family: var(--main-font);
+}
+.quote{
+    margin: 0em 2em 2em;
+    border-left: 5px solid var(--third-color);
+    padding-left: 1em;
+}
+.container_author_genre{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    width: 80%;
+    background: var(--main-color);
+    color: var(--white-color);
+    height: 7em;
+    cursor: pointer;
+}
+.genre{
+    color: var(--second-color);
+    font-size: 12px;
+}
+.fa-long-arrow-right{
+    margin-right: 1em;
+}
+.author_genre{
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    padding-left: 1em;
+}
+footer{
+    color: var(--second-color);
+    font-family: var(--footer-font);
+    font-size: 10px;
+    display: flex;
+    justify-content: center;
+    position: absolute;
+    bottom: 0;
+    right: 30%;
+}
+@media (min-width:768px){
+    .quote {
+        width: 50%;
+    }
+    .container_author_genre {
+        width: 50%;
+    }
+    .author {
+        justify-content: center;
+    }
+    footer{
+        color: var(--second-color);
+        font-family: var(--footer-font);
+        font-size: 10px;
+        display: flex;
+        justify-content: center;
+        position: absolute;
+        bottom: 0;
+        right: 30%;
+    }
 }
 </style>
